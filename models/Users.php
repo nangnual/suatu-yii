17183d39eb1def293845fed0d3523201959101b9
Yanuar Arifin <3326885+nangnual@users.noreply.github.com>
2018-02-20 23:53:32 +0700

    generate CRUD users

---
+++ models/Users.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $email
+ * @property string $password
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['email', 'password'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'email' => 'Email',
+            'password' => 'Password',
+        ];
+    }
+
+    
+    public static function makePassword($password)
+    {
+        $tempPass = md5(trim($password));
+        $finalPass = sha1($tempPass.substr($tempPass, 4,10));
+        return $finalPass;
+    }
+
+}
