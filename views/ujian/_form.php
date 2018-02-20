7a1451a8c3deb7011e41e24f200f9c72e4df0405
Yanuar Arifin <3326885+nangnual@users.noreply.github.com>
2018-02-20 21:46:14 +0700

    ganti activeform pakai kartik

--- views/ujian/_form.php
+++ views/ujian/_form.php
@@ -1,7 +1,8 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+// use yii\widgets\ActiveForm;
+use kartik\widgets\ActiveForm;
 use kartik\datetime\DateTimePicker;
 
 /* @var $this yii\web\View */
@@ -15,7 +16,16 @@ use kartik\datetime\DateTimePicker;
 
     <?= $form->field($model, 'nama_test')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'waktu_test')->textInput() ?>
+    <?= $form->field($model, 'waktu_test')->widget(DateTimePicker::className(), [
+    	   // 'name' => $model->waktu_test,
+	    'options' => ['placeholder' => 'Select operating time ...'],
+	    // 'convertFormat' => true,
+	    'pluginOptions' => [
+	        'format' => 'yyyy-mm-dd H:i:s',
+	        // 'startDate' => '01-Mar-2014 12:00 AM',
+	        'todayHighlight' => true
+    	]
+    	]);?>
 
     <?= $form->field($model, 'durasi_test')->textInput() ?>
 
