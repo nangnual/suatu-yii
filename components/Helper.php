f7a2455fc090f36aeac48f1a6876509ef33b8376
Yanuar Arifin <3326885+nangnual@users.noreply.github.com>
2018-02-22 22:06:25 +0700

    init Helper untuk fungsi yang dipakai dimana2

---
+++ components/Helper.php
@@ -0,0 +1,13 @@
+<?php 
+namespace app\components;
+
+class Helper {
+	
+    public static function makePassword($password)
+    {
+        $tempPass = md5(trim($password));
+        $finalPass = sha1($tempPass.substr($tempPass, 4,10));
+        return $finalPass;
+    }
+}
+ ?>
