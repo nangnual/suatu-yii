56034371078fd1b047f6652bf345a68c2cb001b7
Yanuar Arifin <3326885+nangnual@users.noreply.github.com>
2018-02-23 19:34:48 +0700

    helper tokenujian

--- components/Helper.php
+++ components/Helper.php
@@ -9,5 +9,10 @@ class Helper {
         $finalPass = sha1($tempPass.substr($tempPass, 4,10));
         return $finalPass;
     }
+
+    public static function makeTokenUjian($initial)
+    {
+    	return sha1($initial);
+    }
 }
  ?>
